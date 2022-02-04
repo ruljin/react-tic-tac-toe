727f7a2ea84880f4d5d132584079013ccc6b7825
fkuca <fkuca91@gmail.com>
2022-02-04 09:00:33 +0100

    refactor(board): add Board component

---
+++ src/components/Board.jsx
@@ -0,0 +1,21 @@
+import { Square } from './Square';
+
+export const Board = ({ squares, onClick }) => (
+	<div>
+		<div className='board-row'>
+			<Square value={squares[0]} onClick={() => onClick(0)} />
+			<Square value={squares[1]} onClick={() => onClick(1)} />
+			<Square value={squares[2]} onClick={() => onClick(2)} />
+		</div>
+		<div className='board-row'>
+			<Square value={squares[3]} onClick={() => onClick(3)} />
+			<Square value={squares[4]} onClick={() => onClick(4)} />
+			<Square value={squares[5]} onClick={() => onClick(5)} />
+		</div>
+		<div className='board-row'>
+			<Square value={squares[6]} onClick={() => onClick(6)} />
+			<Square value={squares[7]} onClick={() => onClick(7)} />
+			<Square value={squares[8]} onClick={() => onClick(8)} />
+		</div>
+	</div>
+);
